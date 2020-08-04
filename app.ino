d58971663cca5485dedf54907558cb25fae33943
Miikka Tuominen <tuommii@gmail.com>
2020-08-04 19:26:21 +0300

    More secure

--- app.ino
+++ app.ino
@@ -1,12 +1,18 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 
-const char	*ssid = "EDIT";
-const char	*pass = "EDIT";
-int			status = WL_IDLE_STATUS;
-int			code;
-String		payload;
-HTTPClient	http;
+const char		*ssid = "EDIT";
+const char		*pass = "EDIT";
+int				status = WL_IDLE_STATUS;
+int				code;
+unsigned int	counter = 0;
+int				ledState = LOW;
+unsigned long	prevMillis = 0;
+unsigned long	currMillis = 0;
+String			msg = String("MSG #");
+String			payload;
+String			response;
+HTTPClient		http;
 
 void setup()
 {
@@ -36,22 +42,45 @@ void setup()
 	Serial.println("\nWifi connected!");
 	Serial.println(WiFi.localIP());
 	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, ledState);
+}
+
+void blinkLed(int interval) {
+	currMillis = millis();
+	if (currMillis - prevMillis < interval)
+		return ;
+	prevMillis = currMillis;
+	if (ledState == LOW)
+	{
+		ledState = HIGH;
+	}
+	else
+	{
+		ledState = LOW;
+	}
+	digitalWrite(LED_BUILTIN, ledState);
 }
 
 void loop()
 {
+	blinkLed(1000);
 	http.begin("http://192.168.1.105:8000/hello");
 	http.addHeader("Content-Type", "text/plain");
+	http.addHeader("Secret", "Jing Yang");
 
-	code = http.POST("Hello from Wemos D1 Mini!");
-	payload = http.getString();
+	counter++;
+	payload = msg + counter;
+	if (counter >= 1000)
+	{
+		counter = 0;
+	}
+
+	code = http.POST(payload);
+	response = http.getString();
 	http.end();
 
 	Serial.println(code);
-	Serial.println(payload);
+	Serial.println(response);
 
-	digitalWrite(LED_BUILTIN, HIGH);
-	delay(5000);
-	digitalWrite(LED_BUILTIN, LOW);
-	delay(5000);
+	delay(420);
 }
--- main.go
+++ main.go
@@ -1,29 +1,51 @@
 package main
 
 import (
-	"io/ioutil"
+	"flag"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
+const errResponse = "Stop that!\n"
+
 func hello(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		w.Write([]byte("Hello!"))
-	case "POST":
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		log.Println(string(body))
-		w.Write([]byte("Got It!"))
+	now := time.Now()
+
+	if r.Header.Get("User-Agent") != "ESP8266HTTPClient" || r.Header.Get("Secret") != "Jing Yang" {
+		w.Write([]byte(errResponse))
+		log.Println("Header not valid")
+		return
 	}
+
+	if r.Method != "POST" {
+		w.Write([]byte(errResponse))
+		log.Println("Method not valid")
+		return
+	}
+
+	body := io.LimitReader(r.Body, 4096)
+	_, err := io.Copy(os.Stdout, body)
+	fmt.Println()
+
+	if err != nil {
+		w.Write([]byte(errResponse))
+		log.Println("Body not valid")
+		return
+	}
+
+	log.Println("Took: ", time.Since(now))
+	w.Write([]byte("Got It!"))
 }
 
 func main() {
+	port := flag.String("port", "8000", "port to listen")
+	flag.Parse()
 	http.HandleFunc("/hello", hello)
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	if err := http.ListenAndServe(":"+*port, nil); err != nil {
 		log.Fatal(err)
 	}
 }
